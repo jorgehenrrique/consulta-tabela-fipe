bcdcf426c2de429f1458d2b7a0d2c8c356ee8435
Jorge H <jorgehenrrique@live.com>
2023-04-17 12:47:03 -0300

    reajuste loader

--- assets/js/script-led.js
+++ assets/js/script-led.js
@@ -3,7 +3,7 @@ const loadLed = document.querySelectorAll('#led');
 function enableElements(index) {
     if (index < loadLed.length) {
         loadLed[index].classList.add('load-led');
-        setTimeout(() => enableElements(index + 1), 200);
+        setTimeout(() => enableElements(index + 1), 90);
     }
 }
 enableElements(0);
